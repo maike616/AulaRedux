908b8d40da90c4632600f713af8e9c3a8e37f275
marcosccp04 <marcos.augusto@icomp.ufam.edu.br>
2023-07-22 10:06:06 -0400

    add userRequistion

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
-import "./index.css";
 import { doLogin } from "../../redux/features/api.login.slice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../redux/store";
-//import 'bootstrap/dist/css/bootstrap.min.css'
+import { AppDispatch, RootState } from "../../redux/store";
+import "./index.css";
 
 export default function Login() {
-    const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>(); //essa linha
+  const navigate = useNavigate();
+
+  const stateLogin = useSelector((state: RootState) => state.login);
+
   const [email, SetEmail] = useState("");
   const [password, SetPassword] = useState("");
-  function TryLogin(){
+
+  // essa função
+  function TryLogin() {
     dispatch(
-        doLogin({
-            email:email,
-            password:password,
-        })
-    )
+      doLogin({
+        email: email,
+        password: password,
+      })
+    );
   }
+
+  useEffect(() => {
+    if (stateLogin.isSucess) {
+      navigate("/home");
+    }
+  }, [stateLogin.isSucess]);
 
   return (
     <form style={{ width: "300px" }} className="container">
@@ -25,10 +37,16 @@ export default function Login() {
         <h6>Loja Virtual</h6>
       </div>
 
-      <input placeholder="Digite seu email" onChange={(e)=>SetEmail(e.target.value)}/>
-      <input placeholder="Digite sua senha" onChange={(e)=>SetPassword(e.target.value)}/>
+      <input
+        placeholder="Digite seu email"
+        onChange={(e) => SetEmail(e.target.value)}
+      />
+      <input
+        placeholder="Digite sua senha"
+        onChange={(e) => SetPassword(e.target.value)}
+      />
 
-      <Button onClick={()=>TryLogin()}>Login</Button>
+      <Button onClick={() => TryLogin()}>Login</Button>
     </form>
   );
 }
